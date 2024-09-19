aad609a79c6f17afe91d0c6678342da8a0be5e42
enum3rat3 <jaiminjadvani3728@gmail.com>
2024-09-19 16:42:38 +0530

    Practicals added

---
+++ Hands-on-list-II/P20/p20.c
@@ -0,0 +1,42 @@
+/*
+Name: Jaimin Jadvani
+Roll No.: MT2024064
+Program Description: Write two programs so that both can communicate by FIFO -Use one way communication.
+
+Date: 19 Sept 2024
+*/
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<stdlib.h>
+
+int main()
+{	
+    int fifoStatus;                              
+    char data[] = "Hello from 20a.c program to 20b.c";
+    int fileDescriptor;                         
+    char *fifoFile = "./FIFO_File";               
+    int writeBytes;
+
+    fifoStatus = mkfifo(fifoFile, S_IRWXU);
+
+    if (fifoStatus == -1)
+        perror("Error: \n");
+    fileDescriptor = open(fifoFile, O_WRONLY);
+    if (fileDescriptor == -1)
+        perror("Error: \n");
+    else
+    {
+        writeBytes = write(fileDescriptor, &data, sizeof(data));
+        if (writeBytes == -1)
+            perror("Error: \n");
+        close(fileDescriptor);
+    }
+}
+
+/*
+
+*/
